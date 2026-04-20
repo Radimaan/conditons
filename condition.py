6d9467b32dc11ee09f2a16252671907c9931c26a
Radwan Hassan <radhassan555@gmail.com>
2026-04-20 09:15:19 +0300

    addj

--- condition.py
+++ condition.py
@@ -1,10 +1,21 @@
-marks = 88
-if marks >= 90:
-    print("Grade: A")
-elif marks >= 80:
-    print("Grade: B") 
-elif marks >= 70:
-    print("Grade: C")
-elif marks >= 60:
-    print("Grade: D")
-else:    print("Grade: F")
+# marks = 8
+# if marks >= 90:
+#     print("Grade: A")
+# elif marks >= 80:
+#     print("Grade: B") 
+# elif marks >= 70:
+#     print("Grade: C")
+# elif marks >= 60:
+#     print("Grade: D")
+# else:    print(type("Grade: F"))
+
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+    vote = input("Do you want to vote? (yes/no): ")
+    if vote.lower() == "yes":
+        print("Thank you for voting!")
+    else:
+        print("You chose not to vote.")
+        print("please vote next time.")
+else:    print("You are not eligible to vote.")
